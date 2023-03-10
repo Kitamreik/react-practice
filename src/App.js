d39d99fd0081f95b4f0ec31322a48b4ebce9a780
kitamreik <kitdamreik@gmail.com>
2023-03-09 20:33:49 -0700

    Rendering a List- Period 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+
+function App() {
+  // define the array of objects
+const lists = [
+  {id: 1,
+  name: "Kit",
+  location: "Denver, CO",
+  followers: 100,
+  following: 50}, 
+  {id: 2,
+    name: "User",
+    location: "Location Pending",
+    followers: 1,
+    following: 1}
+  ];
+  return (
+    <div className="App">
+      {lists.map(list => (
+        <ul>
+          <li>Name: {list.name}</li>
+          <li>Location: {list.location}</li>
+          <li>Followers: {list.followers}</li>
+          <li>Following: {list.following}</li>
+        </ul>
+      )
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+// Boilerplate code
+/*
 function App() {
   return (
     <div className="App">
@@ -22,4 +57,4 @@ function App() {
   );
 }
 
-export default App;
+*/ 
